89b76c031dc0af240a7c0d9fda24f53b58a96642
German Pinilla <gpinilladev@gmail.com>
2021-09-04 10:48:14 -0500

    make it better

--- controllers/user.js
+++ controllers/user.js
@@ -160,23 +160,8 @@ const userSignIn = (req, res) => {
 // };
 
 const userTest = (req, res) => {
-  // console.log('req: ', req);
   let params = req.body;
   console.log('params: ', params);
-
-  // let temperaturaAmbiente = req.params["tempamb"];
-  // let temperaturaInicial = req.params["tempini"];
-  // let temperaturaProgramada = req.params["tempsetpoint"];
-  // let fecha = req.params["fecha"];
-  // let proceso = req.params["proceso"];
-
-  // let params = {
-  //   "temperaturaAmbiente": temperaturaAmbiente,
-  //   "temperaturaInicial": temperaturaInicial,
-  //   "temperaturaProgramada": temperaturaProgramada,
-  //   "proceso": proceso,
-  // };
-
   res.status(200).send({ mensaje: "Datos recibidos", datosqueenviaste: params });
 }
 
